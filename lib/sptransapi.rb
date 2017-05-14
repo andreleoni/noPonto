d71aa7b15b8bab9c389d0c32bcff6a4718e57d61
André Luiz Leoni <andreluizleoni@gmail.com>
2017-05-14 20:32:13 -0300

    lib completa da sptrans

--- lib/sptransapi.rb
+++ lib/sptransapi.rb
@@ -14,15 +14,57 @@ module SptransModule
     end
 
     def authenticate_with_api
-      @api_access.post("#{self.access_uri}/Login/Autenticar?token=#{self.token}",{})
+      @api_access.post("#{self.access_uri}/Login/Autenticar?token=#{self.token}", {})
     end
 
-    def to_search_lines(search_terms)
-      @api_access.get("#{self.access_uri}/Linha/Buscar?termosBusca=#{search_terms}",{})
+    #### Linhas #####
+
+    def lines_to_search(search_terms)
+      @api_access.get("#{self.access_uri}/Linha/Buscar?termosBusca=#{search_terms}", {})
     end
+
+    def lines_load_details(line_code)
+      @api_access.get("#{self.access_uri}/Linha/CarregarDetalhes?codigoLinha=#{line_code}", {})
+    end
+
+    #### Paradas #####
 
     def stop_search(search_terms)
-      @api_access.get("#{self.access_uri}/Parada/Buscar?termosBusca=#{search_terms}",{})
+      @api_access.get("#{self.access_uri}/Parada/Buscar?termosBusca=#{search_terms}", {})
+    end
+
+    def stop_search_by_line(line_code)
+      @api_access.get("#{self.access_uri}/Parada/BuscarParadasPorLinha?codigoLinha=#{line_code}", {})
+    end
+
+    def stop_search_by_hall(hall_code)
+      @api_access.get("#{self.access_uri}/Parada/BuscarParadasPorCorredor?codigoCorredor=#{hall_code}", {})
+    end
+
+    #### Corredores #####
+
+    def halls
+      @api_access.get("#{self.access_uri}/Corredor", {})
+    end
+
+    #### Posição Dos Veículos #####
+
+    def vehicles_position(line_code)
+      @api_access.get("#{self.access_uri}/Posicao?codigoLinha=#{line_code}", {})
+    end
+
+    #### Previsão De Chegada ####
+
+    def arrive_preview_on_stop_by_line(stop_code, line_code)
+      @api_access.get("#{self.access_uri}/Previsao?codigoParada=#{stop_code}&codigoLinha=#{line_code}", {})
+    end
+
+    def arrive_preview_on_stop_all_points(line_code)
+      @api_access.get("#{self.access_uri}/Previsao/Linha?codigoLinha=#{line_code}", {})
+    end
+
+    def arrive_preview_on_stop(stop_code)
+      @api_access.get("#{self.access_uri}/Previsao/Parada?codigoParada=#{stop_code}", {})
     end
   end
 end
--- spec/lib/sptransapi_spec.rb
+++ spec/lib/sptransapi_spec.rb
@@ -5,36 +5,126 @@ TOKEN_CONS = '68c53a13edefd596e8fde8044e9589a76f002fd74866a456b3313ec0815968ed'
 
 RSpec.describe SptransModule::Call do
   describe "POST test authorize" do
-    it "Api response == 200" do
+    it "Autenticação e Credenciais - Autenticação com sucesso" do
       instance = SptransModule::Call.new(TOKEN_CONS)
 
+      expect(instance.authenticate_with_api.status).to eq(200)
       expect(instance.authenticate_with_api.body).to eq("true")
     end
-  end
 
-  describe "POST test not authorized" do
-    it "Api response == 200" do
+    it "Autenticação e Credenciais - Autenticação com falha" do
       instance = SptransModule::Call.new("asdasdas")
+
+      expect(instance.authenticate_with_api.status).to eq(200)
       expect(instance.authenticate_with_api.body).to eq("false")
     end
   end
 
-  describe "GET to_search lines" do
-    it "Api response == 200" do
+  describe "GET Linhas" do
+    it "Buscar - Api response == 200" do
       instance = SptransModule::Call.new(TOKEN_CONS)
-      response = instance.to_search_lines(8000)
+
+      search_terms = 8000
+      response = instance.lines_to_search(search_terms)
+
+      expect(JSON.parse(response.body).is_a? Array).to eq(true)
+      expect(response.status).to eq(200)
+    end
+
+    it "CarregarDetalhes - Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+
+      line_code = 8000
+      response = instance.lines_load_details(line_code)
 
       expect(JSON.parse(response.body).is_a? Array).to eq(true)
       expect(response.status).to eq(200)
     end
   end
 
-  describe "GET stop_search" do
-    it "Api response == 200" do
+  describe "GET Paradas" do
+    it "Buscar - Api response == 200" do
       instance = SptransModule::Call.new(TOKEN_CONS)
-      response = instance.stop_search(8000)
+
+      search_terms = 8000
+      response = instance.stop_search(search_terms)
 
       expect(JSON.parse(response.body).is_a? Array).to eq(true)
+      expect(response.status).to eq(200)
+    end
+
+    it "BuscarParadasPorLinha - Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+
+      line_code = 8000
+      response = instance.stop_search_by_line(line_code)
+
+      expect(JSON.parse(response.body).is_a? Array).to eq(true)
+      expect(response.status).to eq(200)
+    end
+
+    it "BuscarParadasPorCorredor - Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+
+      hall_code = 8000
+      response = instance.stop_search_by_hall(hall_code)
+
+      expect(JSON.parse(response.body).is_a? Array).to eq(true)
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "GET Corredores" do
+    it "Corredores - Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+
+      response = instance.halls
+
+      expect(JSON.parse(response.body).is_a? Array).to eq(true)
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "GET Posição Dos Veículos" do
+    it "PosicaoDosVeiculos - Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+      line_code = 8000
+      response = instance.vehicles_position(line_code)
+
+      expect(JSON.parse(response.body).is_a? Hash).to eq(true)
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "GET Previsão De Chegada" do
+    it "PrevisaoDeChegada - Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+
+      stop_code = 4200953
+      line_code = 8000
+      response = instance.arrive_preview_on_stop_by_line(stop_code, line_code)
+
+      expect(JSON.parse(response.body).is_a? Hash).to eq(true)
+      expect(response.status).to eq(200)
+    end
+
+    it "Previsao De Chegada De Uma Linha em Todos os Pontos- Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+
+      line_code = 8000
+      response = instance.arrive_preview_on_stop_all_points(line_code)
+
+      expect(JSON.parse(response.body).is_a? Hash).to eq(true)
+      expect(response.status).to eq(200)
+    end
+
+    it "Previsao De Chegada De Todas as Linhas em um Ponto - Api response == 200" do
+      instance = SptransModule::Call.new(TOKEN_CONS)
+
+      stop_code = 4200953
+      response = instance.arrive_preview_on_stop(stop_code)
+
+      expect(JSON.parse(response.body).is_a? Hash).to eq(true)
       expect(response.status).to eq(200)
     end
   end
