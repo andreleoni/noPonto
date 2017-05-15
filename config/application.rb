e7e1988bfcdb7ce5326226dfcb60b120d013400d
Paulo Carmino <paulocarmino@MacBook-Pro-de-Paulo.local>
2017-05-15 12:15:31 -0300

    Create tests and actions in Lines_Controller

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require './lib/sptransapi'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module NoPonto
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    ENV['TOKEN_CONS'] = '68c53a13edefd596e8fde8044e9589a76f002fd74866a456b3313ec0815968ed'
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get '/search_lines/:term', to: 'lines#search_lines'
+  get '/load_line_details/:line_code', to: 'lines#load_line_details'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
