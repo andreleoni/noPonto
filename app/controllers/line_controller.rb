f7ce5d3025881e37f09ef4431d2f4c6aed01d6ea
Daniel Oliveira <daniel_oliveira0@hotmail.com>
2017-05-15 22:50:18 -0300

    Parse token ENV[]

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -3,13 +3,13 @@ require 'json'
 class LineController < ApplicationController
 
   def search_lines
-    api = SptransModule::Call.new(ENV["TOKEN_CONS"])
+    api = SptransModule::Call.new(ENV['SPTRANS_TOKEN'])
     response = api.lines_to_search(params[:term])
     render json: response
   end
 
   def load_line_details
-    api = SptransModule::Call.new(ENV["TOKEN_CONS"])
+    api = SptransModule::Call.new(ENV['SPTRANS_TOKEN'])
     response = api.lines_load_details(params[:line_code])
     render json: response
   end
--- app/controllers/stop_controller.rb
+++ app/controllers/stop_controller.rb
@@ -2,13 +2,13 @@ require 'json'
 
 class StopController < ApplicationController
   def stop_search
-    api = SptransModule::Call.new(ENV["TOKEN_CONS"])
+    api = SptransModule::Call.new(ENV['SPTRANS_TOKEN'])
     response = api.stop_search(params[:term])
     render json: response
   end
 
   def stop_search_by_line
-    api = SptransModule::Call.new(ENV["TOKEN_CONS"])
+    api = SptransModule::Call.new(ENV['SPTRANS_TOKEN'])
     response = api.stop_search_by_line(params[:line_code])
     render json: response
   end
