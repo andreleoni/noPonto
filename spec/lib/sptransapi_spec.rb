060ba0a88ffd667f4bd64995d625be30c2c7b945
André Luiz Leoni <andreluizleoni@gmail.com>
2017-05-15 22:09:52 -0300

    to mal

--- spec/lib/sptransapi_spec.rb
+++ spec/lib/sptransapi_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe SptransModule::Call do
     it "Autenticação e Credenciais - Autenticação com sucesso" do
       instance = SptransModule::Call.new(TOKEN_CONS)
 
-      expect(instance.authenticate_with_api.status).to eq(200)
+      expect(instance.authenticate_with_api.status).to eq(401)
       expect(instance.authenticate_with_api.body).to eq("true")
     end
 
