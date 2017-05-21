1748dc52ba18a5417e700d234a4ca4f8f58ddeba
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-05-21 00:38:06 -0700

    Edited routes for login, logout, register

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root 'home#index'
 
   post 'home/search_lines' => 'home#search_lines'
