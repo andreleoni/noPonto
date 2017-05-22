6ce9f6d6e122d4ac86ede16ecf3e2e51d8fd4595
Ondrej Kubala <kub.ond@gmail.com>
2017-05-22 20:03:43 -0300

    Adds modals for signing up and for displaying favorite lines.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  has_many :favorites
 
  def self.from_omniauth(auth)
  		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
